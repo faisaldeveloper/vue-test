5fac374492afaf686007a5dc40582edbb0f01e24
Faisal Developer <faisal.shahzad786@gmail.com>
2020-08-10 09:58:22 +0500

    Added few basic components and added bootstrap-vue

---
+++ src/components/Common.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="main-common">
+
+    <p>{{ msg2 }} </p>
+
+    <div class="demo" 
+    @click="attachRed = !attachRed" 
+    :class="{red: attachRed}">
+    </div>
+
+    <div class="demo" 
+    @click="attachGreen = !attachGreen" 
+    :class="{green: attachGreen}">
+    </div>
+
+    <div class="demo" 
+    @click="attachYellow = !attachYellow" 
+    :class="{yellow: attachYellow}">
+    </div> 
+
+    <div class="demo" 
+    :class="dynamic">
+    </div>
+
+    <hr>
+
+    <input type="text" name="color" v-model="dynamic">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Common",
+  prop:{
+    msg2: 'Common Tasks'
+  },
+  data(){
+    return {
+      //msg2 : "whats going on guys.",
+      attachRed: false,
+      attachGreen: false,
+      attachYellow: false,
+      dynamic: "blue",
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.demo{
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  display: inline-block;
+  margin:10px;
+}
+
+.red{
+  background-color: red;
+}
+.green{
+  background-color: green;
+}
+.yellow{
+  background-color: yellow;
+}
+.blue{
+  background-color: blue;
+}
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-content">
+    <h1>{{ msg }}</h1> 
+    <Todo />
+    <Common />
+  </div>
+</template>
+
+<script>
+import Todo from "./Todo.vue";
+import Common from "./Common.vue";
+
+export default {
+  name: "Content",
+  props: {
+    msg: String
+  },
+  components: {
+    Todo,
+    Common
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
